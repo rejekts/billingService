a5fe0ff6442e97d189e1d3495ccaf9d676be85df
James Dempsey <james.dempsey420@gmail.com>
2019-05-20 16:33:35 -0500

    stripe integrations

---
+++ dbstructure.js
@@ -0,0 +1,16 @@
+billingTable = {
+  id: "primary key",
+  userid: '"foreign" key',
+  token: null,
+  hastoken: "true/false",
+  name: "",
+  // cardNumber: "",
+  // expMonth: "",
+  // expYear: "",
+  //^^^Stored as token
+  billingAddress1: "",
+  billingAddress2: "",
+  billingCity: "",
+  billingState: "",
+  billingZip: ""
+};
--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
+require("dotenv").config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
-
-const port = process.env.PORT || 3000;
+const stripe = require("stripe")(process.env.SECRET_KEY);
+const port = process.env.PORT || 8000;
 app.use(express.static("dist"));
 app.use(
   bodyParser.json({
     strict: false
   })
+);
+console.log(
+  (async () => {
+    const charge = await stripe.charges.create({
+      amount: 999,
+      currency: "usd",
+      source: "tok_visa",
+      receipt_email: "jenny.rosen@example.com"
+    });
+    console.log(charge);
+  })()
 );
 
 app.listen(port, () => {
